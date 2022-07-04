6104a421e4869accea43097a6cf13135d44c161c
Ahmed elsayed <87664326+Ahmedelsayed265@users.noreply.github.com>
2022-07-05 00:20:02 +0200

    1

--- app.js
+++ app.js
@@ -3,48 +3,49 @@ const musicList = [
     name: "After Dark",
     artist: "Mr.Kitty",
     poster: "poster-1",
-    src: "file-1",
+    audioSrc: "file-1",
   },
   {
     name: "Bekya",
     artist: "Donia Waelll & El Waili",
     poster: "poster-2",
-    src: "file-2",
+    audioSrc: "file-2",
   },
   {
     name: "Wahdy Lakn",
     artist: "Mohammed Saeed",
     poster: "poster-3",
-    src: "file-3",
+    audioSrc: "file-3",
   },
   {
     name: "Without Me",
     artist: "Eminem",
     poster: "poster-4",
-    src: "file-4",
+    audioSrc: "file-4",
   },
   {
     name: "Venom",
     artist: "Eminem",
     poster: "poster-5",
-    src: "file-5",
+    audioSrc: "file-5",
   },
   {
     name: "One Kiss",
     artist: "Calvin Harris, Dua Lipa",
     poster: "poster-6",
-    src: "file-6",
+    audioSrc: "file-6",
   },
   {
     name: "Bawazan",
     artist: "Dj Totti X almas",
     poster: "poster-7",
-    src: "file-7",
+    audioSrc: "file-7",
   },
 ];
 const imgWrapper = document.querySelector(".music_photo .poster img"),
   audioName = document.querySelector(".music_photo .caption h4"),
-  audioArtist = document.querySelector(".music_photo .caption p");
+  audioArtist = document.querySelector(".music_photo .caption p"),
+  mainAudio = document.querySelector("#main-audio");
 let musicIndex = 1;
 window.addEventListener("load", () => {
   loadMusic(musicIndex);
@@ -54,4 +55,5 @@ function loadMusic(indexNum) {
   audioName.innerHTML =
     musicList[indexNum].name + " - " + musicList[indexNum].artist;
   audioArtist.innerHTML = musicList[indexNum].artist;
+  mainAudio.src = `sounds/${musicList[indexNum].audioSrc}.mp3`;
 }
